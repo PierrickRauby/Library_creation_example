dce566241ec56e6e563d6197860587de0816394e
PierrickRauby <pierrick.rauby@gadz.org>
2018-02-06 12:40:11 -0500

    modifying gcc -l option, working for some functions in libadd & libprod

--- codes/main.c
+++ codes/main.c
@@ -14,8 +14,8 @@ int main()
 
 
 printf("Adding to d gives  %i\n",addition(d) );
-printf("Substraction to d gives %i\n", substraction(d) );
-printf("Multiplying d gives %i\n",multiplication(d) );
+//printf("Substraction to d gives %i\n", substraction(d) );
+//printf("Multiplying d gives %i\n",multiplication(d) );
 printf("Dividing d gives %i\n",division(d) );
 
 return 0;
